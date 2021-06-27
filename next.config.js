598ec20830320e98655d19abea169280562394b0
Roberto Borghesi <dborghez@yahoo.it>
2021-06-27 22:00:43 +0200

    First commit

--- next.config.js
+++ next.config.js
@@ -1,3 +1,30 @@
+const path = require('path')
+
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  
+  webpack(config) {
+    const newConfig = config
+
+    newConfig.resolve.modules.push(path.resolve('./'))
+
+    newConfig.module.rules.push({
+      test: /\.ya?ml$/,
+      type: 'json',
+      exclude: /node_modules/,
+      use: ['yaml-loader'],
+    })
+
+    return newConfig
+  },
+
+  async exportPathMap() {
+    const routes = {
+      '/': { page: '/' },
+    }
+
+    return routes
+  },
+
 }
