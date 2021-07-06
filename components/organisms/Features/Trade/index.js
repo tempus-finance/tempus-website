4b2167faaf2b313f0756f0a5f548bb0c5904ac95
Roberto Borghesi <dborghez@yahoo.it>
2021-07-06 14:15:19 +0200

    [Features]: add trade animation

--- components/organisms/Features/Trade/index.js
+++ components/organisms/Features/Trade/index.js
@@ -1,124 +1,12 @@
 import React from 'react'
 
 import Single from '../Single'
+import Cylinder from './Cylinder'
 
 export default React.memo(function Trade({data}) {
   return (
     <Single data={data}>
-
-      <svg
-        viewBox='0 0 251 341'
-        xmlns="http://www.w3.org/2000/svg">
-        <defs>
-          <path
-            d="M0 0v135.56c0 11.568 7.393 23.1 22.188 31.916 29.6 17.557 78.023 17.557 107.613 0C144.597 158.698 152 147.146 152 135.56V0H0z"
-            id="path-1"/>
-          <path
-            d="M0 0v31.24c0 5.632 3.697 11.263 11.09 15.535 14.759 8.546 39.043 8.546 53.802 0 7.402-4.272 11.098-9.894 11.098-15.534V0H0z"
-            id="path-3"/>
-        </defs>
-        <g
-          id="Design"
-          stroke="none"
-          strokeWidth="1"
-          fill="none"
-          fillRule="evenodd">
-          <g
-            id="Desktop-HD_wireframes"
-            transform="translate(-944 -1444)">
-            <g
-              id="Group"
-              transform="translate(945 1444)">
-              <g id="Bar#2-Copy">
-                <path
-                  d="M76.125 214.742c-19.508 0-39.016 4.389-53.81 13.158-29.6 17.557-29.6 46.286 0 63.843 14.794 8.779 34.302 13.168 53.81 13.168 19.508 0 39.007-4.389 53.811-13.168 29.59-17.557 29.59-46.286 0-63.843-14.804-8.769-34.303-13.158-53.81-13.158z"
-                  id="Shadow"
-                  fill="#000"
-                  fillRule="nonzero"/>
-                <g
-                  id="Body_big"
-                  transform="translate(.117 86.094)">
-                  <mask
-                    id="mask-2"
-                    fill="#fff">
-                    <use xlinkHref="#path-1"/>
-                  </mask>
-                  <use
-                    id="Path"
-                    stroke="#000"
-                    strokeWidth="2.16"
-                    fill="#FFDF99"
-                    xlinkHref="#path-1"/>
-                  <path
-                    id="Rectangle"
-                    fill="#000"
-                    fillRule="nonzero"
-                    opacity=".25"
-                    mask="url(#mask-2)"
-                    d="M-18.501-159.908h63.696v380.333h-63.696z"/>
-                  <path
-                    id="Rectangle"
-                    fill="#000"
-                    fillRule="nonzero"
-                    mask="url(#mask-2)"
-                    d="M-41.489-159.908h63.696v380.333h-63.696z"/>
-                </g>
-                <path
-                  d="M76.125 129.534c-20.236 0-39.187-4.62-53.361-13.047C8.742 108.17 1.016 97.118 1.016 85.374c0-11.745 7.726-22.77 21.748-31.086C36.938 45.88 55.889 41.25 76.125 41.25c20.237 0 39.178 4.62 53.362 13.038 14.021 8.317 21.738 19.406 21.738 31.123 0 11.717-7.717 22.796-21.738 31.113-14.184 8.38-33.134 13.01-53.362 13.01z"
-                  id="Top_big"
-                  stroke="#000"
-                  strokeWidth="2.16"
-                  fill="#FFECC2"
-                  fillRule="nonzero"/>
-              </g>
-              <g
-                transform="translate(98.001 43.157)"
-                id="Bar#2-Copy-2">
-                <path
-                  d="M76.116 296.43c-10.037 0-19.427-2.231-26.451-6.297-6.863-3.967-10.649-9.202-10.649-14.752 0-5.55 3.786-10.786 10.649-14.744 7.024-4.065 16.414-6.296 26.451-6.296 10.038 0 19.436 2.23 26.46 6.296 6.863 3.967 10.65 9.203 10.65 14.762 0 5.559-3.787 10.794-10.65 14.752-7.024 4.048-16.422 6.279-26.46 6.279z"
-                  id="Shadow"
-                  stroke="#000"
-                  strokeWidth="2.16"
-                  fill="#000"
-                  fillRule="nonzero"/>
-                <g
-                  id="Body_small"
-                  transform="translate(38.126 199.83)">
-                  <mask
-                    id="mask-4"
-                    fill="#fff">
-                    <use xlinkHref="#path-3"/>
-                  </mask>
-                  <path
-                    stroke="#000"
-                    strokeWidth="2.16"
-                    d="M74.91 1.08v30.16c0 2.663-.901 5.316-2.679 7.804-1.771 2.48-4.403 4.79-7.88 6.797-7.232 4.186-16.799 6.263-26.36 6.263-9.562 0-19.13-2.077-26.361-6.264-6.948-4.015-10.55-9.272-10.55-14.6h0V1.08h73.83z"/>
-                  <path
-                    id="Rectangle"
-                    fill="#000"
-                    fillRule="nonzero"
-                    opacity=".25"
-                    mask="url(#mask-4)"
-                    d="M-9.255-112.576h31.848V72.546H-9.255z"/>
-                  <path
-                    id="Rectangle"
-                    fill="#000"
-                    fillRule="nonzero"
-                    mask="url(#mask-4)"
-                    d="M-20.749-112.576h31.848V72.546h-31.848z"/>
-                </g>
-                <path
-                  d="M76.116 220.87c-10.037 0-19.427-2.231-26.451-6.297-6.863-3.967-10.649-9.202-10.649-14.752 0-5.55 3.786-10.786 10.649-14.744 7.024-4.066 16.414-6.296 26.451-6.296 10.038 0 19.436 2.23 26.46 6.296 6.863 3.967 10.65 9.202 10.65 14.762 0 5.559-3.787 10.794-10.65 14.752-7.024 4.048-16.422 6.278-26.46 6.278z"
-                  id="Top_small"
-                  stroke="#000"
-                  strokeWidth="2.16"
-                  fill="#FFF"
-                  fillRule="nonzero"/>
-              </g>
-            </g>
-          </g>
-        </g>
-      </svg>
+      <Cylinder />
     </Single>
   )
 })
