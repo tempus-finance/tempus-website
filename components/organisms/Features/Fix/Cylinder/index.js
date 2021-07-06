2e692e9355589aed77405c094eb5b8ab1bf272b5
Roberto Borghesi <dborghez@yahoo.it>
2021-07-06 17:28:33 +0200

    [Fix]: add animation

--- components/organisms/Features/Fix/Cylinder/index.js
+++ components/organisms/Features/Fix/Cylinder/index.js
@@ -13,12 +13,12 @@ const Root = styled.svg`
 
 export default React.memo(function CylinderBig(props){
   const {heightBig = 30, heightSmall = 30} = props
-  const id = 'trade'
-  const color = 'violet'
-  const resetGapSmall = 15
-  const smallShiftFromSvg = 75
-  const globalTransformOrigin = "130px 348px"
-  const smallTransformOrigin = "60px 160px"
+  const id = 'fix'
+  const color = 'green'
+  const resetGapSmall = 23
+  const smallShiftFromSvg = 80
+  const globalTransformOrigin = "180px 348px"
+  const smallTransformOrigin = "130px 280px"
 
   const ref = useRef()
   const tl = useRef(gsap.timeline())
@@ -55,7 +55,6 @@ export default React.memo(function CylinderBig(props){
     small.top = elm.getElementById("Top_small"),
     small.body = elm.getElementById("Body_small"),
     small.all = elm.getElementById("Small_all"),
-    small.shadow = elm.getElementById("Small_shadow"),
     small.strings = {
       mask: "",
       path: ""
@@ -68,7 +67,7 @@ export default React.memo(function CylinderBig(props){
     },
 
     small.updateMask = function (v) {
-      let ns = this.strings.mask.replaceAll("0v16.25", `${v}v${16.25 - v}`)
+      let ns = this.strings.mask.replaceAll("0v23.19", `${v}v${23.19 - v}`)
       ns = ns.replaceAll("V0", `V${v}`)
       this.mask.setAttribute("d", ns)
 
@@ -76,13 +75,12 @@ export default React.memo(function CylinderBig(props){
     },
 
     small.updatePath = function (v) {
-      let ns = this.strings.path.replaceAll("1v15.25", `${v}v${15.25 - v}`)
+      let ns = this.strings.path.replaceAll("1v22.19", `${v}v${22.19 - v}`)
       ns = ns.replaceAll("V1", `V${v}`)
       this.path.setAttribute("d", ns)
 
       return ns
     }
-
 
     small.strings.mask = small.mask.getAttribute("d")
     small.strings.path = small.path.getAttribute("d")
@@ -97,7 +95,6 @@ export default React.memo(function CylinderBig(props){
     big.top = elm.getElementById("Top_big"),
     big.body = elm.getElementById("Body_big"),
     big.all = elm.getElementById("Big_all"),
-    big.shadow = elm.getElementById("Big_shadow"),
     big.strings = {
       mask: "",
       path: ""
@@ -107,15 +104,16 @@ export default React.memo(function CylinderBig(props){
       mask: 0,
       path: 0
     },
+
     big.updateMask = function (v) {
-      let ns = this.strings.mask.replaceAll("0v99.31", `${v}v${99.31 - v}`)
+      let ns = this.strings.mask.replaceAll("0v52.46", `${v}v${52.46 - v}`)
       ns = ns.replaceAll("V0", `V${v}`)
       this.mask.setAttribute("d", ns)
 
       return ns
     },
     big.updatePath = function (v) {
-      let ns = this.strings.path.replaceAll("1v98.31", `${v}v${98.31 - v}`)
+      let ns = this.strings.path.replaceAll("1v51.42", `${v}v${51.42 - v}`)
       ns = ns.replaceAll("V1", `V${v}`)
       this.path.setAttribute("d", ns)
 
@@ -147,34 +145,8 @@ export default React.memo(function CylinderBig(props){
     setInitialValues()
 
     const global = ref.current.getElementById('All')
-
-    const bigX = -60
-    const bigY = bigX * 0.5
-
-    const smallX = bigX - 60
-    const smallY = -smallX * 0.5
-    const smallFinalY = (heightBig + smallShiftFromSvg + smallY * 1.1)
-
-    const smallShadowX = smallX * 0.49
-    const smallShadowY = smallY * 0.49
-
     const sm = {p: -heightSmall}
     const bg = {p: -heightBig}
-
-
-
-
-    const getGrowTl = ({big: b, small: s}) => {
-      const tl = gsap.timeline({delay: 1.4})
-
-      tl
-        .to(sm, { p: s, duration: 1.2, ease: 'Power1.easeInOut' }, 0)
-        .to(small.top, { y: s, duration: 1.2, ease: 'Power1.easeInOut' }, 0)
-        .to(bg, { p: b, duration: 1.2, ease: 'Power1.easeInOut' }, 0)
-        .to(big.top, { y: b, duration: 1.2, ease: 'Power1.easeInOut'}, 0)
-
-      return tl
-    }
 
     tl.current = gsap.timeline({
       delay: 1,
@@ -187,29 +159,32 @@ export default React.memo(function CylinderBig(props){
       },
     })
 
+    const createGrowTl = (v) => {
+      const tl = gsap.timeline({delay: 0.5})
+
+      tl
+        .to(bg, { p:v, duration: 1.2, ease: 'Power2.easeInOut' }, 0)
+        .to(big.top,{ y:v, duration: 1.2, ease: 'Power2.easeInOut' }, 0)
+
+      return tl
+    }
+
     tl.current
       //
       .to(global, {scale: 1, duration: 2, ease: 'Power2.easeOut'})
       .to(ref.current, {opacity: 1, duration: 0.3}, 0)
+      .addLabel('enter:end')
+      .to(small.top, {duration: 1.4, y: resetGapSmall, strokeWidth: 1.3, fill: fill.bigTop, ease: 'Power1.easeInOut'}, 'enter:end')
+      .to(small.path, {duration: 1.4, strokeWidth: 1.3, ease: 'Power1.easeInOut'}, 'enter:end')
+      .to(sm, {duration: 1.4, p: resetGapSmall, ease: 'Power1.easeInOut'}, 'enter:end')
+      .to(small.all, {duration: 1.4,  scale: 1.6813, transformOrigin: '116px 281px', y: 47, ease: 'Power1.easeInOut'}, 'enter:end')
+      .to(small.all, {duration: 0.6, opacity: 0})
+      .set(small.all, {display: 'none'})
+      .addLabel('morph:end', '-=0.2')
+      .add(createGrowTl(-70))
+      .add(createGrowTl(-110))
+      .add(createGrowTl(-150))
 
-      .addLabel('scale:end', '-=0.2')
-      .to(small.all, {duration: 1.6, x: `+=${smallX}`, y: `+=${smallY}`, ease: 'Power1.easeInOut'}, 'scale:end')
-      .to(small.shadow, {duration: 1.6, x: `+=${smallShadowX}`, y: `+=${smallShadowY}`, ease: 'Power1.easeInOut'}, 'scale:end')
-      .to([big.all, big.shadow], {duration: 1.6, x: `-=${bigX}`, y: bigY, ease: 'Power1.easeInOut'}, 'scale:end')
-      .to(small.all, {duration: 1.6,y: `+=${smallFinalY}`, ease: 'Power1.easeInOut'})
-
-      .addLabel('growing:start')
-      .fromTo(sm, {p:sm.p}, { p: 10, duration: 1.4, ease: 'Power1.easeInOut' }, 'growing:start')
-      .fromTo(small.top, {y:sm.p}, { y: 10, duration: 1.4, ease: 'Power1.easeInOut' }, 'growing:start')
-      .fromTo(bg, {p:bg.p}, { p: -60, duration: 1.4, ease: 'Power1.easeInOut' }, 'growing:start')
-      .fromTo(big.top, {y:bg.p}, { y: -60, duration: 1.4, ease: 'Power1.easeInOut' }, 'growing:start')
-
-      .add(getGrowTl({small: -190, big: 90}))
-      .add(getGrowTl({small: -70, big: -100}))
-      .add(getGrowTl({small: -210, big: 40}))
-      .add(getGrowTl({small: -110, big: -140}))
-
-    tl.current.timeScale(1.4)
   }
 
   return (
@@ -222,10 +197,10 @@ export default React.memo(function CylinderBig(props){
     >
       <defs>
         <path
-          d="M0 0v99.31c0 10.7 7 21.39 21.07 29.5 28.09 16.22 74.07 16.22 102.17 0 14-8.11 21.07-18.8 21.07-29.5V0H0z"
+          d="M0 0v52.46c0 18.72 12.34 37.53 37 51.72 49.32 28.47 130 28.47 179.32 0 24.65-14.23 37-33 37-51.76V0H0z"
           id={idBigMaskName}/>
         <path
-          d="M0 0v16.25C0 22.17 3.89 28.1 11.68 32.6c15.57 9 41.05 9 56.63 0 7.78-4.5 11.68-10.43 11.68-16.35V0H0z"
+          d="M0 0v23.19C0 34.34 7.33 45.5 22 53.96c29.31 16.92 77.27 16.92 106.59 0 14.65-8.46 22-19.62 22-30.77V0H0z"
           id={idSmallMaskName}/>
       </defs>
       <g
@@ -235,19 +210,14 @@ export default React.memo(function CylinderBig(props){
         fill="none"
         fillRule="evenodd">
         <path
-          d="M130.16 516c-18.52 0-37 4.06-51.09 12.17-28.09 16.22-28.09 42.77 0 59 14 8.11 32.57 12.17 51.09 12.17 18.52 0 37-4.06 51.09-12.17 28.1-16.22 28.1-42.77 0-59-14.05-8.11-32.57-12.17-51.09-12.17z"
-          id="Big_shadow"
-          fill="#000000"
-          fillRule="nonzero"/>
-        <path
-          d="M130.162 534.67c-10.266 0-20.51 2.24-28.32 6.717-15.57 8.953-15.57 23.607 0 32.566 7.76 4.476 18.054 6.717 28.32 6.717 10.266 0 20.51-2.24 28.32-6.717 15.576-8.953 15.576-23.607 0-32.566-7.789-4.476-18.054-6.717-28.32-6.717z"
-          id="Small_shadow"
+          d="M129.65 452c-32.5 0-65 7.12-89.66 21.35-49.32 28.47-49.32 75.07 0 103.54 24.65 14.23 57.16 21.35 89.66 21.35s65-7.12 89.67-21.35c49.31-28.47 49.31-75.07 0-103.54-24.66-14.26-57.16-21.35-89.67-21.35z"
+          id="Shadow"
           fill="#000000"
           fillRule="nonzero"/>
         <g id="Big_all">
           <g
             id="Body_big"
-            transform="translate(58 401.89)">
+            transform="translate(3 422.82)">
             <mask
               id={bigMaskName}
               fill="white">
@@ -255,26 +225,26 @@ export default React.memo(function CylinderBig(props){
             </mask>
             <path
               id="Big_path"
-              fill={fill.bigBody}
               stroke="#000000"
+              fill={fill.bigBody}
               strokeWidth="2"
-              d="M143.31 1v98.31c0 10.42-6.982 20.763-20.57 28.634-27.825 16.061-73.355 16.061-101.17 0-6.753-3.892-11.85-8.384-15.286-13.194C2.767 109.825 1 104.571 1 99.31h0V1h142.31z"/>
+              d="M252.32 1v51.42c0 9.364-3.153 18.725-9.427 27.502-6.095 8.526-15.126 16.495-27.073 23.392-49.045 28.311-129.275 28.311-178.321 0-11.941-6.872-20.967-14.832-27.06-23.35C4.156 71.182 1 61.813 1 52.46h0V1h251.32z"/>
             <path
               id="Rectangle"
               fill="#000000"
               fillRule="nonzero"
               opacity=".25"
               mask={`url(#${bigMaskName})`}
-              d="M-17.56-173.74h60.47v351.48h-60.47z"/>
+              d="M-30.83-426.76H75.31v616.85H-30.83z"/>
             <path
               id="Rectangle"
               fill="#000000"
               fillRule="nonzero"
               mask={`url(#${bigMaskName})`}
-              d="M-39.39-173.74h60.47v351.48h-60.47z"/>
+              d="M-69.14-426.76H37v616.85H-69.14z"/>
           </g>
           <path
-            d="M130.16 442.55c-19.18 0-37.15-4.27-50.59-12C66.31 422.86 59 412.69 59 401.88c0-10.81 7.31-21 20.57-28.63 13.44-7.76 31.41-12 50.59-12 19.18 0 37.15 4.27 50.59 12 13.27 7.66 20.57 17.83 20.57 28.63s-7.3 21-20.57 28.64c-13.44 7.76-31.41 12.03-50.59 12.03z"
+            d="M129.65 492.28c-33.8 0-65.46-7.54-89.16-21.22C17 457.47 4 439.39 4 420.15c0-19.24 13-37.31 36.49-50.9C64.19 355.57 95.85 348 129.65 348s65.47 7.54 89.17 21.22c23.53 13.59 36.48 31.66 36.48 50.9s-13 37.32-36.48 50.91c-23.7 13.71-55.37 21.25-89.17 21.25z"
             id="Top_big"
             stroke="#000000"
             strokeWidth="2"
@@ -283,7 +253,7 @@ export default React.memo(function CylinderBig(props){
           <g id="Small_all">
             <g
               id="Body_small"
-              transform="translate(90.17 312.1)">
+              transform="translate(54.37 314.45)">
               <mask
                 id={smallMaskName}
                 fill="white">
@@ -291,26 +261,26 @@ export default React.memo(function CylinderBig(props){
               </mask>
               <path
                 id="Small_path"
-                fill={fill.smallBody}
                 stroke="#000000"
+                fill={fill.smallBody}
                 strokeWidth="2"
-                d="M78.99 1v15.25c0 5.64-3.813 11.223-11.18 15.484-7.653 4.42-17.739 6.616-27.819 6.616-10.08 0-20.163-2.195-27.81-6.616C4.802 27.472 1 21.89 1 16.25h0V1h77.99z"/>
+                d="M149.59 1v22.19c0 10.87-7.262 21.682-21.5 29.904-29.045 16.761-76.555 16.761-105.59 0-7.05-4.066-12.378-8.76-15.97-13.785C2.85 34.164 1 28.679 1 23.19h0V1h148.59z"/>
               <path
                 id="Rectangle"
                 fill="#000000"
                 fillRule="nonzero"
                 opacity=".25"
                 mask={`url(#${smallMaskName})`}
-                d="M-9.74-235.11h33.52V59.72H-9.74z"/>
+                d="M-18.33-261.67h63.09V105h-63.09z"/>
               <path
                 id="Rectangle"
                 fill="#000000"
                 fillRule="nonzero"
                 mask={`url(#${smallMaskName})`}
-                d="M-21.84-235.11h33.52V59.72h-33.52z"/>
+                d="M-41.1-261.67h63.09V105H-41.1z"/>
             </g>
             <path
-              d="M130.16 334.19c-10.55 0-20.43-2.35-27.82-6.61-7.21-4.16-11.18-9.66-11.18-15.48s4-11.33 11.18-15.49c7.39-4.26 17.27-6.61 27.82-6.61s20.44 2.35 27.84 6.61c7.21 4.16 11.18 9.66 11.18 15.49s-4 11.32-11.18 15.48c-7.4 4.26-17.28 6.61-27.84 6.61z"
+              d="M129.65 358.93c-20 0-38.77-4.46-52.8-12.56-13.85-8-21.48-18.62-21.48-29.9 0-11.28 7.63-21.91 21.48-29.91 14.03-8.1 32.78-12.56 52.8-12.56 20.02 0 38.77 4.46 52.8 12.56 13.86 8 21.49 18.62 21.49 29.91s-7.63 21.9-21.49 29.9c-14.03 8.1-32.78 12.56-52.8 12.56z"
               id="Top_small"
               stroke="#000000"
               strokeWidth="2"
