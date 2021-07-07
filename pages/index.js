0144a7d97c2848f881393236e91cf4133ab5941c
Roberto Borghesi <dborghez@yahoo.it>
2021-07-07 21:30:02 +0200

    [Header]: Compress header on scroll on desktop

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import {useEffect} from 'react'
 import { gsap } from 'gsap'
 import { ScrollTrigger } from 'gsap/ScrollTrigger'
+import throttle from 'lodash-es/throttle'
+import {useStore} from 'store'
+
+
 import {Events} from 'helpers'
 
 import {Seo, Header, Hero, Features, Roadmap, Team, Investors, Faq, JoinUs, Footer} from 'components'
@@ -9,6 +13,8 @@ gsap.registerPlugin(ScrollTrigger)
 import {colors} from 'data'
 
 export default function Home() {
+  const setIsHeaderCompressed = useStore('setIsHeaderCompressed')
+
   useEffect(() => {
     const duration = 1
     const {body, documentElement: html } = document
@@ -23,6 +29,18 @@ export default function Home() {
 
     gsap.to(body, {duration: 1.2, opacity: 1, delay: 0.5})
   },[])
+
+  useEffect(() => {
+    const detectScroll = () => {
+      if(window.scrollY > 100) {
+        setIsHeaderCompressed(true)
+      }else {
+        setIsHeaderCompressed(false)
+      }
+    }
+
+    window.addEventListener('scroll', throttle(detectScroll, 300))
+  }, [])
 
   return (
     <>
