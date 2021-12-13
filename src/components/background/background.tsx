a1895dfa3d3c7444f5af6bd4f35a6a30788f36cc
Lazar Mitic <Lazar__93@hotmail.com>
2021-12-13 12:01:23 +0100

    Replace background mask image with div element

--- src/components/background/background.tsx
+++ src/components/background/background.tsx
@@ -5,11 +5,11 @@ import './background.scss';
 
 const Background = () => (
   <div className="tf__background__container">
-    <div className="tf__background__gradient" />
-    <div className="tf__background__logo">
-      <HeroLogo />
+    <div className="tf__background__gradient">
+      <div className="tf__background__logo">
+        <HeroLogo />
+      </div>
     </div>
-    <div className="tf__background__mask" style={{ backgroundImage: 'url("/images/background-mask.png")' }} />
   </div>
 );
 export default Background;
