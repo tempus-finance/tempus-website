73fe133f8f688a2a38b9b135cebb2712a9a21658
Lazar Mitic <Lazar__93@hotmail.com>
2021-12-13 12:29:21 +0100

    Include fonts in app bundle

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+import './fonts/source-sans-pro/SourceSansPro-Regular.ttf';
+import './fonts/source-sans-pro/SourceSansPro-SemiBold.ttf';
+import './fonts/source-sans-pro/SourceSansPro-Bold.ttf';
+import './fonts/manrope/Manrope-Medium.ttf';
+import './fonts/manrope/Manrope-Bold.ttf';
+
 import './index.scss';
 
 ReactDOM.render(
