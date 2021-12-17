666973386743aaa601bb4d31acbc4c42a204f4cf
Lazar Mitic <Lazar__93@hotmail.com>
2021-12-17 12:34:35 +0100

    Navbar style update (#16)

--- src/components/icons/menuIcon.tsx
+++ src/components/icons/menuIcon.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { FC } from 'react';
+import IconProps from './iconProps';
 
-const MenuIcon = () => (
-  <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M18 12H0V10H18V12ZM18 7H0V5H18V7ZM18 2H0V0H18V2Z" fill="#222222" />
-  </svg>
-);
+const MenuIcon: FC<IconProps> = (props) => {
+  const { fillColor } = props;
+
+  return (
+    <svg width="18" height="12" viewBox="0 0 18 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M18 12H0V10H18V12ZM18 7H0V5H18V7ZM18 2H0V0H18V2Z" fill={fillColor || '#222222'} />
+    </svg>
+  );
+};
 export default MenuIcon;
