a92bf4a864a1131307356df812c4b6540efcebd9
ShawTim <shawtim.liu@gmail.com>
2022-02-19 02:01:01 +0800

    update on TOS and disclaimer (#35)
    * update for website
    
    * update tos and disclaimer
    
    * minor update on tos

--- src/components/disclaimer/disclaimer.tsx
+++ src/components/disclaimer/disclaimer.tsx
@@ -20,13 +20,14 @@ const Disclaimer = () => {
         </Typography>
         <Spacer size={129} type="vertical" />
         <Typography variant="body-text" color="inverted">
-          Tempus Protocol (the “Protocol”) is a decentralized finance application deployed on the Ethereum blockchain
-          that allows suppliers and borrowers of certain cryptoassets to earn yields using smart contracts. The Protocol
-          is made up of free, public, open-source or source-available software. Your use of the Protocol involves
-          various risks, including, but not limited to, losses while cryptoassets are supplied to the Protocol, losses
-          due to the fluctuation of prices of tokens in a pool, and losses due to price slippage and cost. Before using
-          the Protocol, you should review the relevant documentation from our GitHub to ensure you understand how the
-          Protocol works. Additionally, just as you can access email protocols such as SMTP through multiple email
+          Tempus Protocol (the “Protocol”) is a decentralized finance application deployed on the relevant blockchain
+          (which includes any blockchain that Tempus contracts are deployed on, including but not limited to Ethereum
+          and Fantom) that allows suppliers and borrowers of certain cryptoassets to earn yields using smart contracts.
+          The Protocol is made up of free, public, open-source or source-available software. Your use of the Protocol
+          involves various risks, including, but not limited to, losses while cryptoassets are supplied to the Protocol,
+          losses due to the fluctuation of prices of tokens in a pool, and losses due to price slippage and cost. Before
+          using the Protocol, you should review the relevant documentation from our GitHub to ensure you understand how
+          the Protocol works. Additionally, just as you can access email protocols such as SMTP through multiple email
           clients, you can access the Protocol through various web and mobile interfaces. You are responsible for doing
           your own due diligence on these interfaces to understand the fees and risks they present.
           <br />
@@ -34,7 +35,7 @@ const Disclaimer = () => {
           AS DESCRIBED IN THE TEMPUS <Link to="/terms-of-service">TERMS OF SERVICE</Link>, THE PROTOCOL IS PROVIDED “AS
           IS”, AT YOUR OWN RISK, AND WITHOUT WARRANTIES OF ANY KIND. Although Tempus Labs, Inc. (trading as “Tempus”)
           developed much of the code for the Protocol, Tempus does not provide, own, or control the Protocol, which is
-          run by smart contracts deployed on the Ethereum blockchain. Upgrades and modifications to the Protocol are
+          run by smart contracts deployed on the relevant blockchain. Upgrades and modifications to the Protocol are
           managed in a community-driven way by governance by holders of the TEMP governance token. No Tempus developer
           or entity involved in creating the Protocol will be liable for any claims or damages whatsoever associated
           with your use, inability to use, or your interaction with other users of, the Protocol, including any direct,
