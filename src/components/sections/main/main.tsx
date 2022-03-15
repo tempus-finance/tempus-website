edd704633c0d390b02e25bc0642947075ffbb5e5
Vukašin Manojlović <iamvukasin@gmail.com>
2022-03-15 11:05:04 +0100

    Replace external link buttons with <a>

--- src/components/sections/main/main.tsx
+++ src/components/sections/main/main.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import Button from '../../button/button';
+import ExternalLink from '../../common/externalLink';
 import DiscordLogo from '../../icons/discordLogo';
 import Spacer from '../../spacer/spacer';
 import Typography from '../../typography/typography';
@@ -11,14 +12,6 @@ const Main = () => {
     window.scrollTo(0, 0);
   }, []);
 
-  const onLaunchAppClick = () => {
-    window.open('https://app.tempus.finance', '_blank');
-  };
-
-  const onDiscordClick = () => {
-    window.open('https://discord.com/invite/6gauHECShr', '_blank');
-  };
-
   return (
     <div className="tf__main__container">
       <div className="tf__main__title">
@@ -27,10 +20,12 @@ const Main = () => {
       <Spacer size={39} type="vertical" />
 
       <div className="tf__main-button">
-        <Button onClick={onLaunchAppClick} width="206px" height="55px">
-          <Typography variant="get-involved-card-button" clickable>
-            INVEST NOW
-          </Typography>
+        <Button width="206px" height="55px">
+          <ExternalLink href="https://app.tempus.finance">
+            <Typography variant="get-involved-card-button" clickable>
+              INVEST NOW
+            </Typography>
+          </ExternalLink>
         </Button>
       </div>
       <div className="tf__main-button-mobile">
@@ -43,7 +38,11 @@ const Main = () => {
           </Typography>
         </div>
       </div>
-      <div className="tf__main-button-discord" onClick={onDiscordClick} aria-hidden="true">
+      <ExternalLink
+        className="tf__main-button-discord"
+        href="https://discord.com/invite/6gauHECShr"
+        aria-hidden="true"
+      >
         <div className="tf__flex-row-center-v">
           <DiscordLogo color="white" />
           <Spacer size={10} type="horizontal" />
@@ -51,7 +50,7 @@ const Main = () => {
             Discord
           </Typography>
         </div>
-      </div>
+      </ExternalLink>
     </div>
   );
 };
